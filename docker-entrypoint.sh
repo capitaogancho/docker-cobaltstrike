c617c78f0361a7fb269737989ec42d0fdbe33633
ralph <ralphte@mbpro15.local>
2017-07-25 00:11:32 -0400

    updates
    started ssh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cu
 mkdir /root/.ssh
 
 echo ${SSH_KEY} > /root/.ssh/authorized_keys
+service ssh start
 
 cd /opt/cobaltstrike
 /opt/cobaltstrike/key.sh ${COBALTSTRIKE_KEY}
